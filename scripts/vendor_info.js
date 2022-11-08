73f10db4fc84a252559aa2cc19cbd6a921c2b1e1
Mylo <emdoubleu.yu@gmail.com>
2022-11-07 21:35:32 -0800

    added a function in vendor_info.js that sets all the elements in vendor_info_template to the data in a vendor item in the vendor collection.

---
+++ scripts/vendor_info.js
@@ -0,0 +1,12 @@
+function displayCards(collection) {
+
+    db.collection("vendors").doc(collection)
+    .onSnapshot(vendorDoc => {                                                               //arrow notation
+        document.querySelector('.vendor-name').innerHTML = vendorDoc.data().name;
+        document.querySelector('.vendor-desc').innerHTML = vendorDoc.data().description;
+        document.querySelector('.vendor-thumbnail').src = `./images/${vendorDoc.data().thumbnail}.jpg`; //Example: NV01.jpg;                                    //using json object indexing
+    })
+
+}
+
+displayCards("tim_hortons");

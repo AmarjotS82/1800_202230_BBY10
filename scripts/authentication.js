11e15af8d5c181a9d456844262b1d4bfb69fc2b2
Amarjot <amarjot.sangha28@gmail.com>
2022-11-09 09:17:07 -0800

    password working

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -11,8 +11,7 @@ var uiConfig = {
         if (authResult.additionalUserInfo.isNewUser) {         //if new user
             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                     name: user.displayName,                    //"users" collection
-                    email: user.email,
-                    password: user.password                        
+                    email: user.email,                       
                 }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("home.html");       //re-direct to main.html after signup
